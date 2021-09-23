19bb27c65d9d52264ad97be0260643448bb75e70
soulmachine <soulmachine@gmail.com>
2021-09-23 12:53:22 -0700

    Updated dependencies

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='crypto_msg_parser',
-    version="1.0.7",
+    version="1.4.9",
     author="soulmachine",
     description="Parse websocket messages from cryptocurreny exchanges",
     long_description=long_description,
