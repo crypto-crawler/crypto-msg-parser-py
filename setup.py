185a547c96ade3875fdfdcce550b6b4318eb7345
soulmachine <soulmachine@gmail.com>
2022-05-19 20:18:31 +0000

    Upgraded to 2.1.7

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ with open("crypto-msg-parser-ffi/cbindgen.toml", "w",
 
 setup(
     name='crypto_msg_parser',
-    version="2.1.5",
+    version="2.1.7",
     author="soulmachine",
     description="Parse websocket messages from cryptocurreny exchanges",
     long_description=long_description,
